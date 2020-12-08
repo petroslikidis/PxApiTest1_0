22d2c86df5ce21e989f8227d809f4406124a3f1a
petroslikidis <petros.likidis@gmail.com>
2020-12-08 20:07:32 +0100

    Fixed directory separator issue in Linux

--- PxDatasource/PxFileSource.cs
+++ PxDatasource/PxFileSource.cs
@@ -5,6 +5,7 @@ using PCAxis.Menu.Implementations;
 using PCAxis.Paxiom;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Xml.Linq;
 
@@ -27,18 +28,18 @@ namespace PxDatasource
         public IPXModelBuilder GetBuilder(string language, string path)
         {
             var builder = new PCAxis.Paxiom.PXFileBuilder();
-            builder.SetPath(System.IO.Path.Combine($@"wwwroot\db\{ DatabaseId}\", path));
+            builder.SetPath(System.IO.Path.Combine($@"wwwroot{Path.DirectorySeparatorChar}db{Path.DirectorySeparatorChar}{ DatabaseId}{Path.DirectorySeparatorChar}", path));
             return builder;
         }
 
         public Item GetMenu(string db, string language, string path)
         {
-            string menuXMLPath = $@"wwwroot\db\{DatabaseId}\menu.xml";
+            string menuXMLPath = $@"wwwroot{Path.DirectorySeparatorChar}db{Path.DirectorySeparatorChar}{DatabaseId}{Path.DirectorySeparatorChar}menu.xml";
             XDocument doc = XDocument.Load(menuXMLPath);
             XmlMenu menu = new XmlMenu(doc, language,
             m => { m.AlterItemBeforeStorage = item => { item.ID.Selection = System.IO.Path.GetFileName(item.ID.Selection); }; });
 
-            ItemSelection cid = string.IsNullOrEmpty(path) ? new ItemSelection() : new ItemSelection(System.IO.Path.GetDirectoryName(db + "\\" + path), System.IO.Path.GetFileName(path));
+            ItemSelection cid = string.IsNullOrEmpty(path) ? new ItemSelection() : new ItemSelection(System.IO.Path.GetDirectoryName(db + Path.DirectorySeparatorChar + path), System.IO.Path.GetFileName(path));
             menu.SetCurrentItemBySelection(cid.Menu, cid.Selection);
 
             // Check if menu level has been found or not
@@ -49,8 +50,8 @@ namespace PxDatasource
                     //Menu level has not been found in database
                     return new PxMenuItem(null);
                 }
-                else if (((path.ToLower().Contains(".px")) && (!path.Contains(@"\"))) &&
-                          ((menu.CurrentItem.ID.Menu.Contains(@"\") && menu.CurrentItem.ID.Selection == path)))
+                else if (((path.ToLower().Contains(".px")) && (!path.Contains(Path.DirectorySeparatorChar.ToString()))) &&
+                          ((menu.CurrentItem.ID.Menu.Contains(Path.DirectorySeparatorChar.ToString()) && menu.CurrentItem.ID.Selection == path)))
                 {
                     // 1. We have a .PX-file with no path (no \) specified in nodeID
                     // 2. SetCurrentItemBySelection however has found the path to the table and put this in CurrentItem.ID.Selection
